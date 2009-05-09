18960c2a0de637f28ef29a7591f3baf688bd8a3a
Robert Burrell Donkin <rdonkin@apache.org>
2009-05-09 15:18:16 +0000

    Start to consolidate calls into a single MetaData for the Mailbox. IMAP-79 https://issues.apache.org/jira/browse/IMAP-79
    git-svn-id: https://svn.apache.org/repos/asf/james/imap/trunk@773232 13f79535-47bb-0310-9956-ffa450edef68

---
+++ torque/src/main/java/org/apache/james/mailboxmanager/torque/MailboxMetaData.java
@@ -0,0 +1,77 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailboxmanager.torque;
+
+import javax.mail.Flags;
+
+import org.apache.james.imap.mailbox.Mailbox;
+
+/**
+ * Describes the current state of a mailbox.
+ */
+public class MailboxMetaData implements Mailbox.MetaData {
+
+    private final int recentCount;
+    private final long[] recent;
+    private final Flags premanentFlags;
+    private final long uidValidity;
+    
+    
+    public MailboxMetaData(final long[] recent, final Flags premanentFlags, final long uidValidity) {
+        super();
+        if (recent == null) {
+            recentCount = 0;
+        } else {
+            recentCount = recent.length;
+        }
+        this.recent = recent;
+        this.premanentFlags = premanentFlags;
+        this.uidValidity = uidValidity;
+    }
+
+    /**
+     * @see {@link Mailbox.MetaData#countRecent()()}
+     */
+    public int countRecent() {
+        return recentCount;
+    }
+
+    /**
+     * @see {@link Mailbox.MetaData#getPermanentFlags()()}
+     */
+    public Flags getPermanentFlags() {
+        return premanentFlags;
+    }
+
+    /**
+     * @see {@link Mailbox.MetaData#getRecent()}
+     */
+    public long[] getRecent() {
+        return recent;
+    }
+
+    /**
+     * @see {@link Mailbox.MetaData#getUidValidity()}
+     */
+    public long getUidValidity() {
+        return uidValidity;
+    }
+
+}
