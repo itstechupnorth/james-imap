28cf218e4cafc60fa5776f48b6c2e8fec05f0412
Norman Maurer <norman@apache.org>
2010-08-20 19:59:37 +0000

    Add javadocs/license header
    git-svn-id: https://svn.apache.org/repos/asf/james/imap/trunk@987620 13f79535-47bb-0310-9956-ffa450edef68

--- store/src/main/java/org/apache/james/imap/store/UpdatedFlag.java
+++ store/src/main/java/org/apache/james/imap/store/UpdatedFlag.java
@@ -16,10 +16,34 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.imap.store;
 
 import javax.mail.Flags;
 
+/**
+ * Represent a Flag update for a message
+ * 
+ *
+ */
 public class UpdatedFlag {
 
     private final long uid;
@@ -32,14 +56,29 @@ public class UpdatedFlag {
        this.newFlags = newFlags;
     }
     
+    /**
+     * Return the old {@link Flags} for the message
+     * 
+     * @return oldFlags
+     */
     public Flags getOldFlags() {
         return oldFlags;
     }
     
+    /**
+     * Return the new {@link Flags} for the message
+     * 
+     * @return newFlags
+     */
     public Flags getNewFlags() {
         return newFlags;
     }
     
+    /**
+     * Return the uid for the message whichs {@link Flags} was updated
+     * 
+     * @return uid
+     */
     public long getUid() {
         return uid;
     }
